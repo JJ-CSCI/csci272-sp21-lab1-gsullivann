ea00ed46e9c66956e820d826886de312f0b72cf3
gsullivann <GLSULLIVANN@GMAIL.COM>
2021-02-17 20:55:32 +0000

    Fixed my code after our class review so it was correct.

--- main.cpp
+++ main.cpp
@@ -6,38 +6,29 @@
 
 // Write your code here
 class Parasite {
+private:
+  double reproductionRate{0.0};
+  double resistance {0.0};
 public:
-
-  double reproductionRate{0};
-  double resistance;
-  double getReproductionRate() { 
-    return reproductionRate; 
-  }
-  double getResistance() {
-    return resistance;
-  }
-
-Parasite reproduce(reproductionRate, {0.10}); 
-Parasite survive(resistance, {0.05});
-
+  double getReproductionRate() { return reproductionRate; }
+  double getResistance() { return resistance; }
+  void reproduce() { reproductionRate += 0.1; }
+  void survive() { resistance += 0.05; }
 };
 
 class Animal {
+private:
+  double immunity {0.0};
+  unsigned int parasite {1000};
 public:
-
-  double immunity;
-  unsigned int parasites{1000};
-public: 
-  double getImmunity(){
-    return immunity;
-  }
-  double getParasiteNumber() {
-    return parasites;
+  double getImmunity() {return immunity; }
+  unsigned int getParasiteNumber() { return parasite; }
+  void takeDrug() {
+    immunity += 0.07;
+    parasite -= 100;
   }
 
-Animal takeDrug( immunity{0.07}; parasites, {100});
-
-}
+};
 
 //------------------------------
 //   DO NOT MODIFY TEST CASES
